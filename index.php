7ef70efb21b49c451b1162cbab952a8852fd4809
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-02-13 18:10:52 -0700

    Added css and js directories and files

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+
+
+		<title>
+			Bootstrap Challenge
+		</title>
+	</head>
+	<body>
+		<header>
+
+		</header>
+		<main>
+
+		</main>
+		<footer>
+
+		</footer>
+	</body>
+</html>
