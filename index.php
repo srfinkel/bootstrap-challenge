8e124a0532491cff0faf4b2edd473b0be60b3878
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-02-13 22:10:06 -0700

    Added containers, rows, and col

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
 		<meta charset="utf-8"/>
 		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-		<!-- Latest compiled and minified CSS -->
+
+		<!-- Bootstrap Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+				crossorigin="anonymous"/>
 
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
@@ -16,6 +18,9 @@
 		<!-- Font Awesome -->
 		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
 				rel="stylesheet"/>
+
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/styles.css" type="text/css" />
 
 		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -28,7 +33,9 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
 		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
 
 		<title>
 			Bootstrap Challenge
@@ -37,14 +44,53 @@
 	<body>
 		<header>
 			<div class="container">
-
+				<div class="row">
+					<div class="col-md-3">
+						<p class="text-center">LOGO</p>
+					</div>
+					<div class="col-md-3"></div>
+					<div class="col-md-6">
+						<p class="text-center">TOP NAVIGATION</p>
+					</div>
+				</div>
 			</div>
 		</header>
 		<main>
-
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<p class="text-center">WELCOME</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-2 col-md-offset-1">PHOTO</div>
+					<div class="col-md-9">
+						<p class="text-center">TEXT</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-3">
+						<img src="http://placekitten.com/200/300" alt="kitten" class="img-circle">
+					</div>
+					<div class="col-md-3"><img src="http://placekitten.com/200/300" alt="kitten" class="img-circle"></div>
+					<div class="col-md-3"><img src="http://placekitten.com/200/300" alt="kitten" class="img-circle"></div>
+					<div class="col-md-3"><img src="http://placekitten.com/200/300" alt="kitten" class="img-circle"></div>
+					<div class="row">
+						<div class="col-md-3">HEADING/TEXT</div>
+						<div class="col-md-3">HEADING/TEXT</div>
+						<div class="col-md-3">HEADING/TEXT</div>
+						<div class="col-md-3">HEADING/TEXT</div>
+					</div>
 		</main>
 		<footer>
-
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<p class="text-center">COPYRIGHT TEXT</p>
+					</div>
+				</div>
+			</div>
 		</footer>
+		</div>
 	</body>
 </html>
